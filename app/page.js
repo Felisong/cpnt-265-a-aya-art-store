dbfd3d56bd126ad5de897ecae9dde0ca24ef9698
Felisong <carolinahs100@gmail.com>
2024-12-04 00:34:41 -0700

    cleaned up and am leaving it for the night. I made a note for mysel f. hopefully this has been deploying.

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import ImageCarousel from "./components/heroComps/ImageCarousel";
 import ProductSection from "./components/products-section/ProductsSection";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
@@ -8,6 +9,15 @@ export default function Home() {
       <ImageCarousel />
 
       <ProductSection />
+      <Button />
     </>
   );
 }
+
+// hi future me.
+// Good plan of execution tomorrow
+// 1. ask how to custom colors/ fonts in material ui.
+// 2. finish footer
+// 3.import forms into formed webpages
+// 4. vague layout prep for products
+// 5. think about database schemas and see if you can take a look. its ok if not.
