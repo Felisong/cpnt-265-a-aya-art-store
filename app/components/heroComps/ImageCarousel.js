1199c742af5d853edcbc410f1cc01d38e1933fe2
Felisong <carolinahs100@gmail.com>
2024-12-03 23:47:21 -0700

    just saving what i have inside carousel for now, ill have more fun later.

--- app/components/heroComps/ImageCarousel.js
+++ app/components/heroComps/ImageCarousel.js
@@ -1,30 +1,67 @@
-// "src": ,
-//       "alt": "A journal with a pink cute hamster staring at you"
-
-// import { Widgets } from "@mui/icons-material"
-
-//     },
-//     {
-//       "src": "/pokemon-25th-anniversary-mewtwo-and-mew-totes-bag-1.webp",
-//       "alt": "A cloth bag with an illustration of Mewtwo and Mew from pokemon."
-//     },
-//     {
-//       "src": "/kingdom-hearts-tamagotchi-keychain-sora-roxas-double-sided-clear-acrylic-1.webp",
-//       "alt": "2 keychains that look like tomagachis. chibi-fied (cute and chubby cheeked) drawn characters from Kingdom Hearts. The characters are Sora, and Roxas. "
-//     }
-//   ]
-// }
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+
 export default function ImageCarousel() {
+  const images = [
+    {
+      src: "/hamster-collection-reusable-sticker-book-size-a5-50-sheets-stationary-1.webp",
+      alt: "A journal with a pink cute hamster staring at you",
+    },
+    {
+      src: "/pokemon-25th-anniversary-mewtwo-and-mew-totes-bag-1.webp",
+      alt: "A cloth bag with an illustration of Mewtwo and Mew from pokemon.",
+    },
+    {
+      src: "/kingdom-hearts-tamagotchi-keychain-sora-roxas-double-sided-clear-acrylic-1.webp",
+      alt: "2 keychains that look like tomagachis. chibi-fied (cute and chubby cheeked) drawn characters from Kingdom Hearts. The characters are Sora, and Roxas. ",
+    },
+  ];
+  const slides = document.getElementsByClassName("carousel-item");
+  const dots = document.getElementsByClassName("dot");
+  const [position, setPosition] = useState(0);
+  const numOfSlides = slides.length;
+
+  const main = 1;
+
   return (
-    <div className="bg-backDropPink p-4">
-      <Image
-        src="/hamster-collection-reusable-sticker-book-size-a5-50-sheets-stationary-1.webp"
-        alt="A journal with a pink cute hamster staring at you"
-        className="flex w-full lg:w-3/4 mx-auto  lg:border-4 lg:border-backDropDark rounded-xl"
-        width={1300}
-        height={600}
-      />
-    </div>
+    <>
+      <div className="w-full flex flex-col lg:mx-auto">
+        {/* if index === img-id then execute then show item */}
+        <div className="carousel-item carousel-item-visible">
+          <img src={images[0].src} alt={images[0].alt} />
+        </div>
+        {/* <div className="carousel-item">
+          <img src={images[1].src} alt={images[1].alt} />
+        </div>
+        <div className="carousel-item">
+          <img src={images[2].src} alt={images[2].alt} />
+        </div> */}
+        <div className="carousel-actions">
+          <button
+            id="carousel-button-prev"
+            aria-label="Previous"
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+          >
+            &lt;
+          </button>
+          <button id="carousel-button-next" aria-label="Next">
+            &gt;
+          </button>
+        </div>
+        <div className="carousel-dots">
+          <input
+            className="dot selected-dot"
+            type="radio"
+            name="dot"
+            defaultChecked=""
+          />
+          <input className="dot" type="radio" name="dot" />
+          <input className="dot" type="radio" name="dot" />
+        </div>
+      </div>
+    </>
   );
 }
