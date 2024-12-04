146feb3588469e2d08922714ac344a1f1ce5e3a5
Felisong <carolinahs100@gmail.com>
2024-12-03 22:27:05 -0700

    got rid of that carousel, it was NOT working with that. I will change approach.

--- app/components/heroComps/ImageCarousel.js
+++ app/components/heroComps/ImageCarousel.js
@@ -1,17 +1,16 @@
-import Image from "next/image";
+//   const hamster = {
+//     src: "/hamster-collection-reusable-sticker-book-size-a5-50-sheets-stationary-1.webp",
+//     alt: "A journal with a pink cute hamster staring at you",
+//   };
+//   const pokemon = {
+//     src: "/pokemon-25th-anniversary-mewtwo-and-mew-totes-bag-1.webp",
+//     alt: "A cloth bag with an illustration of Mewtwo and Mew from pokemon.",
+//   };
+//   const kingdomHeart = {
+//     src: "/kingdom-hearts-tamagotchi-keychain-sora-roxas-double-sided-clear-acrylic-1.webp",
+//     alt: "2 keychains that look like tomagachis. chibi-fied (cute and chubby cheeked) drawn characters from Kingdom Hearts. The characters are Sora, and Roxas. ",
+//   };
 
 export default function ImageCarousel() {
-  //   const hamster = {
-  //     src: "/hamster-collection-reusable-sticker-book-size-a5-50-sheets-stationary-1.webp",
-  //     alt: "A journal with a pink cute hamster staring at you",
-  //   };
-  //   const pokemon = {
-  //     src: "/pokemon-25th-anniversary-mewtwo-and-mew-totes-bag-1.webp",
-  //     alt: "A cloth bag with an illustration of Mewtwo and Mew from pokemon.",
-  //   };
-  //   const kingdomHeart = {
-  //     src: "/kingdom-hearts-tamagotchi-keychain-sora-roxas-double-sided-clear-acrylic-1.webp",
-  //     alt: "2 keychains that look like tomagachis. chibi-fied (cute and chubby cheeked) drawn characters from Kingdom Hearts. The characters are Sora, and Roxas. ",
-  //   };
-  return <p> meow</p>;
+  return <p> meowww</p>;
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import ImageCarousel from "./components/heroComps/ImageCarousel";
 
 export default function Home() {
   return (
     <>
+      <ImageCarousel />
       <p> image soon :D ?</p>
     </>
   );
