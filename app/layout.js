ce9fcce5839f03b95fb6bb1e9a3831ac20edc108
Felisong <carolinahs100@gmail.com>
2024-12-04 19:14:35 -0700

    saving this before i connect supabase in case anything goes wrong

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Navigation from "./components/Navigation/Navigation";
+import Navigation from "./components/navigation/Navigation";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
