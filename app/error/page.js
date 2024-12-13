e99142114d0cb4555a8ed1cdf9d7cca798b97b4f
Felisong <carolinahs100@gmail.com>
2024-12-13 00:19:48 -0700

    updated error page, looking to get varcel to show up!

--- app/error/page.js
+++ app/error/page.js
@@ -1,3 +1,10 @@
 export default function ErrorPage() {
-  return <p>Sorry, something went wrong</p>;
+  return (
+    <div className="bg-[url('/gemini-generated-background.png')] bg-cover bg-center h-[75vh] flex items-center justify-center relative">
+      <div className="absolute inset-0 bg-black opacity-30"></div>
+      <div className="relative z-10  bg-backDropPink p-8 text-lg rounded shadow-lg flex flex-col">
+        <p>Sorry, something went wrong. TBA</p>
+      </div>
+    </div>
+  );
 }
