22dfae3fcb7e0dda4fac6b300a9bc69b9a5762a1
Felisong <carolinahs100@gmail.com>
2024-12-15 21:37:35 -0700

    added login button to error page.

--- app/error/page.js
+++ app/error/page.js
@@ -1,10 +1,17 @@
+import Link from "next/link";
+
 export default function ErrorPage() {
   return (
     <div className="bg-[url('/gemini-generated-background.png')] bg-cover bg-center h-[75vh] flex items-center justify-center relative">
       <div className="absolute inset-0 bg-black opacity-30"></div>
       <div className="relative z-10  bg-backDropPink p-8 text-lg rounded shadow-lg flex flex-col">
-        <p>Sorry, something went wrong. TBA</p>
+        <p>Sorry, something went wrong.</p>
         <p> Please check your login credentials and try again. </p>
+        <Link href={`/login`}>
+          <button className="bg-buttonPurple text-white p-4 font-bold rounded-full flex mx-auto m-4 hover:">
+            Login Page
+          </button>
+        </Link>
       </div>
     </div>
   );
